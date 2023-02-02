327d064044d7f3cf9f343bb34e1d59468b98fabb
SebastianPereiro <kuzmin_konstantin@bk.ru>
2023-02-02 23:03:20 +0300

    commit3

--- src/test/java/MedicalServiceImplTests.java
+++ src/test/java/MedicalServiceImplTests.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import ru.netology.patient.entity.BloodPressure;
 import ru.netology.patient.entity.HealthInfo;
@@ -25,7 +26,6 @@ public class MedicalServiceImplTests {
 
         SendAlertService alertService = Mockito.mock();
 
-
         BloodPressure currentPressure_WillBeCalled = new BloodPressure(60, 120);
         BloodPressure currentPressure_WillNotBeCalled = new BloodPressure(120,80);
 
@@ -47,20 +47,48 @@ public class MedicalServiceImplTests {
         PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
         Mockito.when(patientInfoRepository.getById("1"))
                 .thenReturn(new PatientInfo("Иван", "Петров", LocalDate.of(1980, 11, 26),
-                        new HealthInfo(new BigDecimal("36.65"), new BloodPressure(120, 80))));
+                        new HealthInfo(new BigDecimal("36.8"), new BloodPressure(120, 80))));
+        Mockito.when(patientInfoRepository.getById("2"))
+                .thenReturn(new PatientInfo("Семен", "Михайлов", LocalDate.of(1982, 1, 16),
+                        new HealthInfo(new BigDecimal("39.9"), new BloodPressure(125, 78))));
 
         SendAlertService alertService = Mockito.mock();
 
         BigDecimal currentTemperature = new BigDecimal("37.9");
 
-        MedicalService medicalService_CallsTheMethod = new MedicalServiceImpl(patientInfoRepository,alertService);
-        medicalService_CallsTheMethod.checkTemperature("1", currentTemperature);
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository,alertService);
+        medicalService.checkTemperature("1", currentTemperature);
+        medicalService.checkTemperature("2", currentTemperature);
 
-        PatientInfo patientInfo = patientInfoRepository.getById("1");
-
+        PatientInfo patientInfo = patientInfoRepository.getById("2");
         String message = String.format("Warning, patient with id: %s, need help", patientInfo.getId());
 
+        Mockito.verify(alertService, Mockito.times(1)).send(message);
+    }
 
-        Mockito.verify(alertService, Mockito.times(0)).send(message);
+    @Test
+    void testCheckBloodPressureMessage(){
+        PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
+        Mockito.when(patientInfoRepository.getById("2"))
+                .thenReturn(new PatientInfo("Семен", "Михайлов", LocalDate.of(1982, 1, 16),
+                        new HealthInfo(new BigDecimal("39.9"), new BloodPressure(125, 78))));
+
+        SendAlertService alertService = Mockito.mock();
+
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+
+        BigDecimal currentTemperature = new BigDecimal("37.9");
+
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository,alertService);
+
+        medicalService.checkTemperature("2", currentTemperature);
+
+        PatientInfo patientInfo = patientInfoRepository.getById("2");
+        String message = String.format("Warning, patient with id: %s, need help", patientInfo.getId());
+
+        Mockito.verify(alertService).send(argumentCaptor.capture());
+
+        Assertions.assertEquals(message, argumentCaptor.getValue());
+
     }
 }
